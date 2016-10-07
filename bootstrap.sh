187062a8b770c0ff4fe5547a75632cd6c291f30f
Asad Moin <moina@advisory.com>
2016-10-07 18:24:26 -0500

    checking in latest vagrant file on lenovo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+apt install software-properties-common
 add-apt-repository ppa:webupd8team/java
 apt-get update
-sudo apt-get install oracle-java8-installer
+apt-get install oracle-java8-installer
 apt-get install -y apache2
-apt-get install maven
+apt install maven
 
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
 fi
+
+cp settings.xml /etc/maven/
+mvn compile

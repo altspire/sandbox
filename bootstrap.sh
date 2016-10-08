fca2bca848aa1fa08a4bc9d80ba10551bc6aad99
unknown <asadmoin@gmail.com>
2016-10-08 14:27:17 -0500

    cleaning up comments and change directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,19 +5,13 @@ add-apt-repository ppa:webupd8team/java
 apt-get update
 apt-get install oracle-java8-installer
 
-# export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
-# export PATH=$JAVA_HOME/bin:$PATH
-
 echo export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre >> /etc/profile.d/java.sh
 echo export PATH=\${JAVA_HOME}/bin:\${PATH} >> /etc/profile.d/java.sh
 
 apt-get install -y apache2
 apt install -y maven
 
-# if ! [ -L /var/www ]; then
-#   rm -rf /var/www
-#   ln -fs /vagrant /var/www
-# fi
-
-# cp settings.xml /etc/maven/
-# mvn compile
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
